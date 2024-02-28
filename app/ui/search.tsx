83b1dfc9d65fc8823a186d274a3cd8e385e6722a
Justin Martz <6052996+JustinMartz@users.noreply.github.com>
2024-02-28 10:14:58 -0700

    Added New Client button to clients.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -7,7 +7,7 @@ export default function ClientSearch() {
             Search
           </label>
           <input
-            className="peer block w-1/2 rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+            className="peer w-3/4 block rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
             placeholder='Search for client'
             
           />
